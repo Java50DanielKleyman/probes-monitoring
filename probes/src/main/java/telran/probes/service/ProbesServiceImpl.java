a52b66707e96fbb451793d3a6bf4ad13a309eec2
Daniel Kleyman <denny_klm1@yahoo.com>
2024-03-10 16:18:33 +0200

    HW-77

--- probes/src/main/java/telran/probes/service/ProbesServiceImpl.java
+++ probes/src/main/java/telran/probes/service/ProbesServiceImpl.java
@@ -1,7 +1,9 @@
 package telran.probes.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -22,7 +24,48 @@ public class ProbesServiceImpl implements ProbesService {
 		List<SensorRange> listOfRanges = rangeRepo.findAll();
 		Map<Long, Range> rangeMap = listOfRanges.stream()
 				.collect(Collectors.toMap(SensorRange::id, SensorRange::range));
-		return null;
+		long randomSensorId = getRandomId(rangeMap);
+		Range range = rangeMap.get(randomSensorId);
+		double randomValue = generateValue(range);
+		long timestamp = System.currentTimeMillis();
+		log.debug("Generated ProbeData with sensor ID {}, value {}, and timestamp {}", randomSensorId, randomValue,
+				timestamp);
+		return new ProbeData(randomSensorId, randomValue, timestamp);
+	}
+
+	private double generateValue(Range range) {
+		double probability = Math.random();
+		double deviationProbability = 0.3;
+		double minValue = range.minValue();
+		double maxValue = range.maxValue();
+		double rangeWidth = maxValue - minValue;
+		if (probability < deviationProbability) {
+			double newValue = generateValueOutsideRange(minValue, maxValue, rangeWidth);
+			log.debug("Generated value outside range: {}", newValue);
+			return newValue;
+		} else {
+			double newValue = minValue + Math.random() * rangeWidth;
+			log.debug("Generated value within range: {}", newValue);
+			return newValue;
+		}
+	}
+
+	private double generateValueOutsideRange(double minValue, double maxValue, double rangeWidth) {
+		double newValue = 0;
+		if (Math.random() < 0.5) {
+			newValue = minValue - Math.random() * rangeWidth;
+		} else {
+			newValue = maxValue + Math.random() * rangeWidth;
+		}
+		return newValue;
+	}
+
+	private long getRandomId(Map<Long, Range> rangeMap) {
+		Random random = new Random();
+		List<Long> idList = new ArrayList<>(rangeMap.keySet());
+		Long randomId = idList.get(random.nextInt(idList.size()));
+		log.debug("Random ID: {}", randomId);
+		return randomId;
 	}
 
 }
