76727e041083e027dc5c484593f70d866efe42be
Daniel Kleyman <denny_klm1@yahoo.com>
2024-03-20 20:26:45 +0200

    HW-80

--- email-notifier/src/main/java/telran/probes/service/EmailsProviderClientService.java
+++ email-notifier/src/main/java/telran/probes/service/EmailsProviderClientService.java
@@ -1,7 +1,7 @@
 package telran.probes.service;
 
 public interface EmailsProviderClientService {
-	String SERVICE_EMAIL = "service-sensors@gmail.com";
+	String SERVICE_EMAIL = "ahtunhe2017777@gmail.com";
 
 	String[] getMails(long sensorId);
 }
